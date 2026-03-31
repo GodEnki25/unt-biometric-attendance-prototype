d867d66c4ab6c20c3c87a6793464953674b0603f
Andrew Kim <Andrewkim13@my.unt.edu>
2026-03-31 12:25:41 -0500

    Add ios

--- react_demo/reactDemo/app/_layout.tsx
+++ react_demo/reactDemo/app/_layout.tsx
@@ -5,6 +5,7 @@ export default function Layout()
 {
     return (
         <Stack initialRouteName="login">
+            <Stack.Screen name="index" options={{ headerShown: false }} />
             <Stack.Screen name="login" options={{ headerShown: false }} />
 
             <Stack.Screen name="dashboard" options={{ headerShown: false }} />
