ad794c387906ae3247073b01e8bceffe4889ea35
Sorel Agbogla <Sorelus25@gmail.com>
2026-04-14 01:22:44 -0500

    improve code organization

--- backend/database/init_db.py
+++ backend/database/init_db.py
@@ -77,7 +77,7 @@ def init_db():
     conn.commit()
     conn.close()
 
-    print("✅ Database initialized successfully.")
+    print("Database initialized successfully.")
 
 if __name__ == "__main__":
     init_db()
